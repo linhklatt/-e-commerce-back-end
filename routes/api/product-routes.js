4b78f5c5fad13b3fe920650caf6f0dcb693b2d1a
linhklatt <112294288+linhklatt@users.noreply.github.com>
2023-01-03 17:28:12 -0800

    fixed the message issue in product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -27,7 +27,7 @@ router.get("/:id", async (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({ message: "No library card found with that id!" });
+      res.status(404).json({ message: "No product found with that id!" });
       return;
     }
 
